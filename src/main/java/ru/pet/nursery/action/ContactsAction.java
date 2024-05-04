76ccc57531498e155ea0ad686fb31dd9b9eede03
Maksim Gogolin <m4xin4t4@gmail.com>
2024-05-04 20:21:43 +0500

    Написан обработчик команд пользователя и заготовки классов для обработки различных команд. Также разработана база данных с таблицами с помощью liquibase. Запрограммированы действия при команде "volunteer" от пользователя.

---
+++ src/main/java/ru/pet/nursery/action/ContactsAction.java
@@ -0,0 +1,12 @@
+package ru.pet.nursery.action;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import java.util.function.BiConsumer;
+
+public class ContactsAction implements BiConsumer<Update, TelegramBot> {
+    @Override
+    public void accept(Update update, TelegramBot telegramBot) {
+
+    }
+}
