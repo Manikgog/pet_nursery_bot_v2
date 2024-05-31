dc9612698a4cf765e0d16886bb4cc29fdba3af13
Роман <urmatos92@gmai.com>
2024-05-31 18:04:05 +0700

    Фикс нотифаер

--- src/main/java/ru/pet/nursery/notifying/Notifier.java
+++ src/main/java/ru/pet/nursery/notifying/Notifier.java
@@ -4,12 +4,15 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.pet.nursery.entity.Volunteer;
 import ru.pet.nursery.web.service.AnimalService;
 import ru.pet.nursery.web.service.ReportService;
+import ru.pet.nursery.web.service.VolunteerService;
 
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -17,11 +20,13 @@ public class Notifier {
     private final TelegramBot telegramBot;
     private final ReportService reportService;
     private final AnimalService animalService;
+    private final VolunteerService volunteerService;
 
-    public Notifier(TelegramBot telegramBot, ReportService reportService, AnimalService animalService) {
+    public Notifier(TelegramBot telegramBot, ReportService reportService, AnimalService animalService, VolunteerService volunteerService) {
         this.telegramBot = telegramBot;
         this.reportService = reportService;
         this.animalService = animalService;
+        this.volunteerService = volunteerService;
     }
 
 
@@ -41,6 +46,12 @@ public class Notifier {
                     "У вас заканчивается адаптационный период "+animal.getPetReturnDate()+" у питомца "+animal.getAnimalName()+" " +
                             ", пожалуйста свяжитесь с волонтером для продления срока пребывания");
             telegramBot.execute(message);
+            List<Volunteer> volunteerList = volunteerService.getAll().stream().filter(Volunteer::isActive).toList();
+            for (Volunteer volunteer : volunteerList) {
+                message = new SendMessage(volunteer.getTelegramUserId(), "У пользователя "
+                        + animal.getUser().getTelegramUserId() + " истек испытательный срок");
+                telegramBot.execute(message);
+            }
         });
     }
 }
