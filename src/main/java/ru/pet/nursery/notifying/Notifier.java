3b9070286a1679de8ef49edd80e22a0059a28be4
Роман <urmatos92@gmai.com>
2024-06-08 00:11:58 +0700

    BagFix Notifier

--- src/main/java/ru/pet/nursery/notifying/Notifier.java
+++ src/main/java/ru/pet/nursery/notifying/Notifier.java
@@ -20,13 +20,14 @@ import ru.pet.nursery.web.service.VolunteerService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class Notifier {
-    private final Logger log = LoggerFactory.getLogger(AdoptedService.class);
+    private final Logger log = LoggerFactory.getLogger(Notifier.class);
     private final TelegramBot telegramBot;
     private final ReportService reportService;
     private final AnimalService animalService;
@@ -48,66 +49,97 @@ public class Notifier {
         this.reportRepo = reportRepo;
     }
 
-
-    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    /**
+     * Метод проверяет ежедневные отчеты по расписанию (1 раз в день).
+     * Отправляет напоминания пользователю 1 раз в день в 20:00 есть ли пропуски по отчету целиком или по его полям.
+     */
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
     public void run() {
+        LocalTime time = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
         log.info("Запущен метод run класса Notifier");
-        List<User> adopters =  animalRepo.findByUserIsNotNull()
+        if (time.equals(LocalTime.of(20, 0))) {
+            List<User> adopters = animalRepo.findByUserIsNotNull()
+                    .stream()
+                    .map(Animal::getUser)
+                    .toList();
+            adopters.stream()
+                    .forEach(u -> {
+                        List<Report> reports = reportRepo.findByUser(u);
+                        if (reports.isEmpty()) {
+                            SendMessage message = new SendMessage(u.getTelegramUserId(), "У вас пока нет ни одного отчёта.");
+                            telegramBot.execute(message);
+                        } else {
+                            reports = reports.stream()
+                                    .filter(r -> r.getReportDate().equals(LocalDateTime.now().minusDays(1).truncatedTo(ChronoUnit.DAYS)))
+                                    .toList();
+                            if (reports.isEmpty()) {
+                                SendMessage message = new SendMessage(u.getTelegramUserId(), "Вы не отправили отчёт за " + LocalDate.now().minusDays(1) + ".");
+                                telegramBot.execute(message);
+                            }
+                            if (!reports.isEmpty()) {
+                                if (!reports.get(0).isBehaviourIsAccepted() ||
+                                        !reports.get(0).isDietIsAccepted() ||
+                                        !reports.get(0).isHealthIsAccepted() ||
+                                        !reports.get(0).isPhotoIsAccepted()) {
+                                    SendMessage message = new SendMessage(u.getTelegramUserId(), "Вы отправили неполный отчёт за " +
+                                            reports.get(0).getReportDate().toLocalDate() + ". Пожалуйста, отправляйте отчёт по всем пунктам.");
+                                    telegramBot.execute(message);
+                                }
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    /**
+     * Метод добавляет по расписанию 1 день к испытательному сроку пребывания питомца если отчета нет в течение суток
+     */
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
+    public void probation() {
+        log.info("Запущен шедулер probation");
+        List<User> adopters = animalRepo.findByUserIsNotNull()
                 .stream()
                 .map(Animal::getUser)
                 .toList();
-
         adopters.stream()
-                        .forEach(u -> {
-                            List<Report> reports = reportRepo.findByUser(u);
-                            if (reports.isEmpty()) {
-                                SendMessage message = new SendMessage(u.getTelegramUserId(), "У вас пока нет ни одного отчёта.");
-                                telegramBot.execute(message);
-                            }else{
-                                reports = reports.stream()
-                                        .filter(r -> r.getReportDate().equals(LocalDateTime.now().minusDays(1).truncatedTo(ChronoUnit.DAYS)))
-                                        .toList();
-                                if(reports.isEmpty()){
-                                    SendMessage message = new SendMessage(u.getTelegramUserId(), "Вы не отправили отчёт за " + LocalDate.now().minusDays(1) + ".");
-                                    telegramBot.execute(message);
-                                }
-                                if(!reports.isEmpty()){
-                                    if(!reports.get(0).isBehaviourIsAccepted() ||
-                                    !reports.get(0).isDietIsAccepted() ||
-                                    !reports.get(0).isHealthIsAccepted() ||
-                                    !reports.get(0).isPhotoIsAccepted()){
-                                        SendMessage message = new SendMessage(u.getTelegramUserId(), "Вы отправили неполный отчёт за " + reports.get(0).getReportDate().toLocalDate() + ". Пожалуйста, отправляйте отчёт по всем пунктам.");
-                                        telegramBot.execute(message);
-                                    }
-                                }
-                            }
-                        });
+                .forEach(u -> {
+                    List<Report> reports = reportRepo.findByUser(u).stream().filter(report -> report.getReportDate()
+                            .toLocalDate().isEqual(LocalDate.now().minusDays(1))).toList();
+                    if (reports.isEmpty()) {
+                        List<Animal> animal = animalRepo.findByUser(u);
+                        animal.get(0).setPetReturnDate(animal.get(0).getPetReturnDate().plusDays(1));
+                    }
+                    if (!reports.isEmpty()) {
+                        if (!reports.get(0).isBehaviourIsAccepted() ||
+                                !reports.get(0).isDietIsAccepted() ||
+                                !reports.get(0).isHealthIsAccepted() ||
+                                !reports.get(0).isPhotoIsAccepted()) {
+                            List<Animal> animal = animalRepo.findByUser(u);
+                            animal.get(0).setPetReturnDate(animal.get(0).getPetReturnDate().plusDays(1));
+                            animalRepo.save(animal.get(0));
+                        }
+                    }
 
-
-
-        reportService.findByPetReturnDate().stream().filter(report ->
-                !report.isPhotoIsAccepted() ||
-                        !report.isBehaviourIsAccepted() ||
-                        !report.isDietIsAccepted() ||
-                        !report.isHealthIsAccepted()
-        ).forEach(report -> {
-            SendMessage message = new SendMessage(report.getUser()
-                    .getTelegramUserId(), "У вас есть пропуск по отчётам. Пожалуйста, отправьте отчёт по животному за просроченный период.");
-            telegramBot.execute(message);
-            report.setReportDate(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES).plusDays(1));
-        });
+                });
     }
 
-    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    /**
+     * Метод отправляет по расписанию 1 раз в день сообщение волонтеру, что у одного из пользователей истек испытательный срок.
+     */
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.DAYS)
     public void completionOfTheAdaptationPeriod() {
+        log.info("Запущен шедулер completionOfTheAdaptationPeriod");
         animalService.adoptionPeriod().forEach(animal -> {
             SendMessage message = new SendMessage(animal.getUser().getTelegramUserId(),
-                    "У вас заканчивается адаптационный период "+animal.getPetReturnDate()+" у питомца "+animal.getAnimalName()+" " +
+                    "У вас заканчивается адаптационный период " +
+                            animal.getPetReturnDate() + " у питомца " +
+                            animal.getAnimalName() + " " +
                             ", пожалуйста свяжитесь с волонтером для продления срока пребывания");
             telegramBot.execute(message);
             List<Volunteer> volunteerList = volunteerService.findIsActive();
             for (Volunteer volunteer : volunteerList) {
-                message = new SendMessage(volunteer.getTelegramUserId(), "У пользователя "
+                message = new SendMessage(volunteer.getTelegramUserId(), "У пользователя @"
                         + animal.getUser().getTelegramUserId() + " истек испытательный срок");
                 telegramBot.execute(message);
             }
